5e36a982df1967bec26bcbc58d1512fda84a32e7
Cooldownn <dphung291@gmail.com>
2018-10-04 02:14:48 +1000

    Add Cocoapods

---
+++ Assignment/Controller/WeatherVC.swift
@@ -0,0 +1,99 @@
+//
+//  WeatherVC.swift
+//  Assignment_iOS
+//
+//  Created by Cooldown on 4/10/18.
+//  Copyright © 2018 Cooldown. All rights reserved.
+//
+
+import UIKit
+
+class WeatherVC: UIViewController {
+    
+    //Constants
+    let WEATHER_URL = "http://api.openweathermap.org/data/2.5/weather"
+    let APP_ID = "e143f386ee9382add79222f7855a46ef"
+    
+    //TODO: Declare instance variables here
+    
+    
+    
+    //Pre-linked IBOutlets
+    @IBOutlet weak var weatherIcon: UIImageView!
+    @IBOutlet weak var cityLabel: UILabel!
+    @IBOutlet weak var temperatureLabel: UILabel!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+        //TODO:Set up the location manager here.
+        
+        
+        
+    }
+    
+    
+    
+    //MARK: - Networking
+    /***************************************************************/
+    
+    //Write the getWeatherData method here:
+    
+    
+    
+    
+    
+    
+    
+    //MARK: - JSON Parsing
+    /***************************************************************/
+    
+    
+    //Write the updateWeatherData method here:
+    
+    
+    
+    
+    
+    //MARK: - UI Updates
+    /***************************************************************/
+    
+    
+    //Write the updateUIWithWeatherData method here:
+    
+    
+    
+    
+    
+    
+    //MARK: - Location Manager Delegate Methods
+    /***************************************************************/
+    
+    
+    //Write the didUpdateLocations method here:
+    
+    
+    
+    //Write the didFailWithError method here:
+    
+    
+    
+    
+    
+    //MARK: - Change City Delegate methods
+    /***************************************************************/
+    
+    
+    //Write the userEnteredANewCityName Delegate method here:
+    
+    
+    
+    //Write the PrepareForSegue Method here
+    
+    
+    
+    
+    
+}
---
+++ Podfile
@@ -0,0 +1,24 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'Assignment_iOS' do
+  # Comment the next line if you're not using Swift and don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for Assignment_iOS
+
+    pod 'SwiftyJSON'
+    pod 'Alamofire'
+    pod 'SVProgressHUD'
+
+  target 'Assignment_iOSTests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+  target 'Assignment_iOSUITests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+end
